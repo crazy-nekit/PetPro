4f9f9ad03d92a5b77a97721433b5a8f30ba08406
crazy-nekit <mcznik228@gmail.com>
2025-03-13 21:58:44 +0200

    Stable Version 2

--- Core/Src/logic.c
+++ Core/Src/logic.c
@@ -8,29 +8,49 @@
 
 extern UART_HandleTypeDef huart1; //Use Uart1 from main.c
 
-static uint8_t rData;
+
+#define BUFF_SIZE 5
+
+static uint8_t rData[BUFF_SIZE];
+static uint8_t ind = 0;
 
  void Logic_Init(void) {
 	 HAL_GPIO_WritePin(GPIOB, Reley_Pin, GPIO_PIN_RESET);
  }
 
  void Logic_StartUART(void){
-	 HAL_UART_Receive_IT(&huart1, &rData, 1);
+	 HAL_UART_Receive_IT(&huart1, &rData[ind], 1);
  }
 
  void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	 if (huart ->Instance==USART1){
-		 if (rData == '1'){
-			 HAL_GPIO_WritePin(GPIOB, Reley_Pin, GPIO_PIN_SET);
-			 HAL_GPIO_WritePin(GPIOB, Led_Pin, GPIO_PIN_SET);
-		 }
-		 else if (rData == '0') {
-			 HAL_GPIO_WritePin(GPIOB, Reley_Pin, GPIO_PIN_RESET);
-			 HAL_GPIO_WritePin(GPIOB, Led_Pin, GPIO_PIN_RESET);
+			 if (rData[ind] == '\n' || rData[ind] == '\r'){
+				 rData[ind] = '\0';
 
-		}
+				 if (strcmp((char*)rData, "on") == 0) {
+				 HAL_GPIO_WritePin(GPIOB, Reley_Pin, GPIO_PIN_SET);
+				 HAL_GPIO_WritePin(GPIOB, Led_Pin, GPIO_PIN_SET);
+			 }
+				 else if (strcmp((char*)rData, "off") == 0) {
+				 HAL_GPIO_WritePin(GPIOB, Reley_Pin, GPIO_PIN_RESET);
+				 HAL_GPIO_WritePin(GPIOB, Led_Pin, GPIO_PIN_RESET);
 
-		 HAL_UART_Receive_IT(&huart1, &rData, 1);
+			}
+
+			memset(rData, 0, BUFF_SIZE);
+			ind = 0;
+
+			 }
+
+			 else {
+				 ind++;
+				 if (ind >= BUFF_SIZE - 1){
+					 ind = 0;
+				 }
+			 }
+
+
+		HAL_UART_Receive_IT(&huart1, &rData[ind], 1 );
 	 }
  }
 
